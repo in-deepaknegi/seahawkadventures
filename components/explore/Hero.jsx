2c0689b6237321765e1bc0a922b132f1770e3498
in-deepaknegi <negideepak.in@gmail.com>
2024-01-15 16:13:13 +0530

    updated hero - explore page

--- components/explore/Hero.jsx
+++ components/explore/Hero.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Hero1 from '@/public/hiking1.WEBP'
 import Hero2 from '@/public/rafting3.WEBP'
@@ -7,21 +8,35 @@ import Hero3 from '@/public/kayak1.WEBP'
 const gallery = [
     {
         id: 1,
+        title: 'Hiking',
         imageSrc: Hero1,
         href: '/explore/rafting',
     },
     {
         id: 2,
         imageSrc: Hero2,
+        title: 'Rafting',
         href: '/explore/rafting',
     },
     {
         id: 3,
         imageSrc: Hero3,
+        title: 'Kayaking',
         href: '/explore/kayaking',
     },
 ]
 const Hero = () => {
+
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    const handleMouseEnter = (index) => {
+        setHoveredIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
+
     return (
         <>
             <section className='bg-white px-6 py-24 lg:py-12 lg:px-8'>
@@ -33,27 +48,33 @@ const Hero = () => {
                         Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
                     </p> */}
                 </div>
-                <div className='mt-16 flex justify-center items-center gap-4 mx-auto max-w-7xl'>
-                    {gallery.map((item) => (
+                <div className='mt-16 md:flex justify-center items-center gap-4 mx-auto max-w-7xl'>
+                    {gallery.map((item, index) => (
                         <figure
                             key={item.id}
-                            className='relative isolate left-0 w-24 h-[20rem] rounded-2xl overflow-hidden transition-all ease-in-out duration-700 shadow-xl flex-[0.33] hover:flex-[2] hover:cursor-pointer hover:rounded-xl'>
+                            onMouseEnter={() => handleMouseEnter(index)}
+                            onMouseLeave={handleMouseLeave}
+                            className='relative isolate left-0 w-full my-8 md:my-0 md:w-24 h-[18rem] md:h-[20rem] rounded-2xl overflow-hidden transition-all ease-in-out duration-700 shadow-xl flex-auto md:flex-[0.33] hover:flex-[2] hover:cursor-pointer hover:rounded-xl'>
                             <Image
                                 src={item.imageSrc}
                                 alt='hero-image-1'
+                                loading='eager'
                                 className='absolute inset-0 -z-10 h-[20rem] object-cover'
                             />
                             <a href={item.href} className='text-xl text-white'>
                                 <span className='absolute inset-0'></span>
-                                Title
+                                {/* Title */}
                             </a>
+                            {hoveredIndex === index && (
+                                <div className='py-1.5 bg-black/60 text-white text-lg px-4 block transition-all ease-in duration-700'>
+                                    {item.title}
+                                </div>
+                            )}
                         </figure>
                     ))}
                 </div>
             </section>
-            <section className='relative isolate px-6 py-24 lg:px-8'>
 
-            </section>
         </>
 
     )
