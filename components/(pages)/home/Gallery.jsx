e592c95d6a6cc56f3b87c9ca5be3ea359e7b263c
negideepak.in@gmail.com <negideepak.in@gmail.com>
2024-03-01 23:11:23 +0530

    minor fixes gallery

--- components/(pages)/home/Gallery.jsx
+++ components/(pages)/home/Gallery.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import Image from 'next/image'
-import Image1 from '@/public/rafting/m00.jpg';
-import Image2 from '@/public/rafting/r00.png';
-import Image3 from '@/public/rafting/r01.jpg';
-import Image4 from '@/public/rafting/r02.jpg';
-import Image5 from '@/public/rafting/r03.jpg';
-import Image6 from '@/public/rafting/r04.jpg';
-import Image7 from '@/public/rafting/r05.jpg';
-import Image8 from '@/public/rafting/r06.jpg';
-import Image9 from '@/public/rafting/r07.jpg';
-import Image10 from '@/public/rafting/r08.jpg';
-import Image11 from '@/public/rafting/r09.jpg';
-import Image12 from '@/public/rafting/r10.jpg';
-import Image13 from '@/public/rafting/r11.jpg';
-import Image14 from '@/public/rafting/r13.jpg';
+import React from "react";
+import Image from "next/image";
+import Image1 from "@/public/rafting/m00.jpg";
+import Image2 from "@/public/rafting/r00.png";
+import Image3 from "@/public/rafting/r01.jpg";
+import Image4 from "@/public/rafting/r02.jpg";
+import Image5 from "@/public/rafting/r03.jpg";
+import Image6 from "@/public/rafting/r04.jpg";
+import Image7 from "@/public/rafting/r05.jpg";
+import Image8 from "@/public/rafting/r06.jpg";
+import Image9 from "@/public/rafting/r07.jpg";
+import Image10 from "@/public/rafting/r08.jpg";
+import Image11 from "@/public/rafting/r09.jpg";
+import Image12 from "@/public/rafting/r10.jpg";
+import Image13 from "@/public/rafting/r11.jpg";
+import Image14 from "@/public/rafting/r13.jpg";
 
 const Gallery = () => {
     return (
-        <section className='relative isolate py-8 lg:py-16'>
-            <div className='max-w-7xl min-h-screen  mx-auto px-8'>
+        <section id="gallery" className="relative isolate py-8 lg:py-16">
+            <div className="max-w-7xl min-h-screen  mx-auto px-8">
                 <div className="flex mb-10 flex-col justify-between gap-y-5 py-4 md:gap-x-10 lg:flex-row">
                     <h2 className="text-4xl font-bold tracking-tight text-black sm:text-5xl">
-                    Image Gallery
+                        Image Gallery
                     </h2>
                     <p className="my-auto max-w-xl text-base leading-8 text-gray-900 md:text-lg">
-                    Explore our beautiful collection of photos featuring rafting and kayaking in Rishikesh.
+                        Explore our beautiful collection of photos featuring rafting and
+                        kayaking in Rishikesh.
                     </p>
                 </div>
 
@@ -34,19 +35,19 @@ const Gallery = () => {
                             alt="Gallery Image"
                             className="h-[50%] object-cover object-center w-full rounded-xl"
                             src={Image1}
-                            loading='eager'
+                            loading="eager"
                         />
                         <Image
                             alt="Gallery Image"
                             className="h-[25%] object-cover object-center w-full rounded-lg"
                             src={Image3}
-                            loading='eager'
+                            loading="eager"
                         />
                         <Image
                             alt="Gallery Image"
                             className="h-[25%] object-cover object-center w-full rounded-lg"
                             src={Image4}
-                            loading='eager'
+                            loading="eager"
                         />
                     </div>
 
@@ -55,13 +56,13 @@ const Gallery = () => {
                             alt="Gallery Image"
                             className="h-[60%] object-cover object-center w-full rounded-xl"
                             src={Image6}
-                            loading='eager'
+                            loading="eager"
                         />
                         <Image
                             alt="Gallery Image"
                             className="h-[40%] object-cover object-center w-full rounded-lg"
                             src={Image7}
-                            loading='eager'
+                            loading="eager"
                         />
                     </div>
 
@@ -70,19 +71,19 @@ const Gallery = () => {
                             alt="Gallery Image"
                             className="h-[50%] object-cover object-center w-full rounded-xl"
                             src={Image8}
-                            loading='eager'
+                            loading="eager"
                         />
                         <Image
                             alt="Gallery Image"
                             className="h-[25%] object-cover object-center w-full rounded-lg"
                             src={Image13}
-                            loading='eager'
+                            loading="eager"
                         />
                         <Image
                             alt="Gallery Image"
                             className="h-[25%] object-cover object-center w-full rounded-lg"
                             src={Image11}
-                            loading='eager'
+                            loading="eager"
                         />
                     </div>
 
@@ -91,13 +92,13 @@ const Gallery = () => {
                             alt="Gallery Image"
                             className="h-[40%] object-cover object-center w-full rounded-xl"
                             src={Image12}
-                            loading='eager'
+                            loading="eager"
                         />
                         <Image
                             alt="Gallery Image"
                             className="h-[60%] object-cover object-center w-full rounded-lg"
                             src={Image10}
-                            loading='eager'
+                            loading="eager"
                         />
                     </div>
                     <div className="flex h-screen flex-col gap-y-4">
@@ -105,19 +106,16 @@ const Gallery = () => {
                             alt="Gallery Image"
                             className="h-[50%] object-cover object-center w-full rounded-xl"
                             src={Image9}
-                            loading='eager'
+                            loading="eager"
                         />
                         <Image
                             alt="Gallery Image"
                             className="h-[40%] object-cover object-center w-full rounded-lg"
                             src={Image14}
-                            loading='eager'
+                            loading="eager"
                         />
-                        <div className='text-black font-semibold'>
-                            Show more ...
-                        </div>
+                        <div className="text-black font-semibold">Show more ...</div>
                     </div>
-
 
                     {/* <div className="flex flex-col space-y-4">
                         <Image
@@ -165,7 +163,7 @@ const Gallery = () => {
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Gallery
+export default Gallery;
